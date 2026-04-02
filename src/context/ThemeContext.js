77596cadc9cae94e0cf6867b921911a7ed4857e2
Carla Nascimento <carlanascimento321567@gmail.com>
2026-04-02 16:55:22 -0300

    criacao do themeArea e finalizacao do ThemeContext.js

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,3 +1,13 @@
-import React from 'react';
+import React, { useState } from "react";
 
-export const ThemeContext = React.createContext(); // ThemeContext.js
+export const ThemeContext = React.createContext();
+
+export const ThemeArea = ({ children, initialTheme }) => {
+  const [theme, setTheme] = useState(initialTheme);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};

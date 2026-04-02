0da6dd1b5e6d8f3b2b26fdf5a2d21fae5fb21633
Carla Nascimento <carlanascimento321567@gmail.com>
2026-04-02 16:56:34 -0300

    finalizando o ContactItem

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -1,9 +1,8 @@
-export const ContactItem = ({ name, theme }) => {
-  let currentLocation = null;
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext.js";
 
-  return (
-    <div>
-      {name}
-    </div>
-  );
+export const ContactItem = ({ name }) => {
+  const { theme } = useContext(ThemeContext);
+
+  return <div className={`contact-item ${theme}`}>{name}</div>;
 };
